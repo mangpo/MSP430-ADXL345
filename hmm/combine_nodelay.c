b06dca4bd8f5e22f4a90cd36523395355bae91d3
mangpo <mangpo@eecs.berkeley.edu>
2016-01-29 17:23:05 -0800

    combine_nodelay: hmm using crystal

--- hmm/combine_nodelay.c
+++ hmm/combine_nodelay.c
@@ -36,21 +36,78 @@
 #define NUM_BYTES_RX 6
 #define ADXL_345     0x53
 
-int RXByteCtr, RPT_Flag = 0;       // enables repeated start when 1
-volatile unsigned char RxBuffer[8];         // Allocate 6 byte of RAM
+int RXByteCtr;       // enables repeated start when 1
+volatile unsigned char RxBuffer[6];         // Allocate 6 byte of RAM
 unsigned char *PRxData;                     // Pointer to RX data
 unsigned char TXByteCtr, RX = 0;
-unsigned char MSData[3];
+unsigned char MSData[2];
 unsigned int wdtCounter = 0;
 
 fp_t acc[3];
 fp_t dir_filter_ref[3];
-fp_t quantizerMap[14][3];
-fp_t b[112];
-fp_t a[8][8];
+
+fp_t quantizerMap[14][3] = {
+  { 12990 , -321 , -7587 }, //0
+  { 7048 , 0 , 7048 },
+  { 0 , 0 , 9967 },
+  { -7048 , 0 , 7048 },
+  { -9967 , 0 , 0 },
+  { -7048 , 0 , -7048 },
+  { 1124 , -264 , -18219 }, //6
+  { 7991 , -34 , -14040 }, //7
+  { 0 , 9967 , 0 },
+  { 0 , 7048 , 7048 },
+  { 0 , -7048 , 7048 },
+  { 0 , -9967 , 0 },
+  { 0 , -7048 , -7048 },
+  { 371 , 7838 , -13185 }}; //d
+
+fp_t b[112] = {
+  1533, 5401, 5209, 3951, 1958, 0, 0, 0, // 0
+  0, 10922, 10922, 10922, 0, 0, 0, 0,
+  0, 0, 10922, 10922, 10922, 0, 0, 0,
+  0, 0, 0, 10922, 10922, 10922, 0, 0,
+  0, 0, 0, 0, 10922, 10922, 10922, 0,
+  0, 0, 0, 0, 0, 10922, 10922, 10922,
+  23533, 8183, 10277, 14808, 15356, 16325, 17108, 17255, // 6
+  7157, 15432, 11460, 3073, 2611, 1647, 615, 0, // 7
+  4, 47, 96, 243, 362, 518, 578, 509,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  538, 3703, 5724, 10690, 12477, 14276, 14466, 15003}; // d
+
+/* // Too big for RAM to have A matrix. T-T
+fp_t a[64] = {
+  10922, 10922, 10922, 0, 0, 0, 0, 0,
+  0, 10922, 10922, 10922, 0, 0, 0, 0,
+  0, 0, 10922, 10922, 10922, 0, 0, 0,
+  0, 0, 0, 10922, 10922, 10922, 0, 0,
+  0, 0, 0, 0, 10922, 10922, 10922, 0,
+  0, 0, 0, 0, 0, 10922, 10922, 10922,
+  0, 0, 0, 0, 0, 0, 16384, 16384,
+  0, 0, 0, 0, 0, 0, 0, 32767};
+
+fp_t b[112] = {
+  1533, 5401, 5209, 3951, 1958, 0, 0, 0, // 0
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 
+  23533, 8183, 10277, 14808, 15356, 16325, 17108, 17255, // 6
+  7157, 15432, 11460, 3073, 2611, 1647, 615, 0, // 7
+  4, 47, 96, 243, 362, 518, 578, 509,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  538, 3703, 5724, 10690, 12477, 14276, 14466, 15003}; // d
+*/
+  
 fp_t f[8];
 fp_t s[8];
-fp_t last_bit = 1 << FP_BITS-1;
 char started = false;
 
 void Setup_TX(unsigned char);
@@ -62,12 +119,13 @@ void Setup_UART();
 void UARTSendArray(unsigned char *TxArray, unsigned char ArrayLength);
 void UARTSendInt(unsigned int x);
 void hmm();
+void run();
 
 int main(void)
 {
   // LED
-  P1DIR |= BIT0;
-  P1OUT |= BIT0;
+  /* P1DIR |= BIT0; */
+  /* P1OUT |= BIT0; */
 
   // UART
   BCSCTL1 = CALBC1_1MHZ; // Set DCO to 1MHz
@@ -88,32 +146,22 @@ int main(void)
   // Init sequence for ADXL345
   //Transmit process
   Setup_TX(ADXL_345);
-  RPT_Flag = 0;
   Transmit(0x2D,0x00);                    // STUCK
   while (UCB0CTL1 & UCTXSTP);             // Ensure stop condition got sent
 
   //Transmit process
   Setup_TX(ADXL_345);
-  RPT_Flag = 0;
   Transmit(0x2D,0x10);
   while (UCB0CTL1 & UCTXSTP);             // Ensure stop condition got sent
 
   //Transmit process
   Setup_TX(ADXL_345);
-  RPT_Flag = 0;
   Transmit(0x2D,0x08);
   while (UCB0CTL1 & UCTXSTP);             // Ensure stop condition got sent
   
-  // Un-comment next block to change range of ADXL345
-  /*
-    Setup_TX(ADXL_345);
-    RPT_Flag = 1;
-    Transmit(0x31,0x01);                            // Range Select at add 0x31 write 0x00 for 2g(default)/ 0x01 for 4g/ 0x02 for 8g/ 0x03 for 16g
-    while (UCB0CTL1 & UCTXSTP);         // Ensure stop condition got sent
-  */
 
   /* Set watchdog timer interval to 1000ms (requires external crystal to work) */
-  WDTCTL = WDT_ADLY_1000;
+  WDTCTL = WDT_ADLY_1_9;
 
   /* "Interrupt enable 1" for the Watchdog Timer interrupt */
   IE1 |= WDTIE;
@@ -125,52 +173,61 @@ int main(void)
   for( ; ; ) { }
 }
 
+char idle = 1;
 
-/* Watchdog Timer interrupt service routine.  The function prototype
- *  tells the compiler that this will service the Watchdog Timer, and
- *  then the function follows.
- *    */
 void watchdog_timer(void) __attribute__((interrupt(WDT_VECTOR)));
 void watchdog_timer(void)
 {
     wdtCounter++;
     /* Count 1 interrupts x 1000ms = 1000ms, or one second */
-    if(wdtCounter == 1) {
-      //Transmit process
-      Setup_TX(ADXL_345);
-      RPT_Flag = 0;
-      TransmitOne(0x32);                      // Request Data from ADXL345
-      while (UCB0CTL1 & UCTXSTP);             // Ensure stop condition got sent
-
-      //Receive process
-      Setup_RX(ADXL_345);
-      Receive();
-      while (UCB0CTL1 & UCTXSTP);             // Ensure stop condition got sent
-
-      acc[0] = ((((int)RxBuffer[1]) << 8) | RxBuffer[0]) << 6;
-      acc[1] = ((((int)RxBuffer[3]) << 8) | RxBuffer[2]) << 6;
-      acc[2] = ((((int)RxBuffer[5]) << 8) | RxBuffer[4]) << 6;
-    
-      // now You have XYZ axis reading in x1,x2,x3 variable....Bingo... you can play with it as you like....
-      // Below if sense x and y angle and Red led is on if its more then 45 or less then -45...
-      // you can put your own condition here...
-
-      /* if ((x1 > 128) || (y1 > 128) || (x1 < -128) || (y1 < -128)) { */
-      /*   P1OUT |= BIT0; // red led on */
-      /* } */
-      /* else { */
-      /*   P1OUT &= ~BIT0; // red led off */
-      /* } */
-
-      Setup_UART();
-      UARTSendArray("sample\n", 7);
-      UARTSendInt(acc[0]);
-      UARTSendInt(acc[1]);
-      UARTSendInt(acc[2]);
-      hmm();
-      /* Reset the counter for the next blink */
+    if(wdtCounter == 2) {
+      if(idle) {
+        idle = 0;
+        run();
+        idle = 1;
+      }
       wdtCounter = 0;
     }
+}
+
+
+/* Watchdog Timer interrupt service routine.  The function prototype
+ *  tells the compiler that this will service the Watchdog Timer, and
+ *  then the function follows.
+ *    */
+void run()
+{
+  //Transmit process
+  Setup_TX(ADXL_345);
+  TransmitOne(0x32);                      // Request Data from ADXL345
+  while (UCB0CTL1 & UCTXSTP);             // Ensure stop condition got sent
+
+  //Receive process
+  Setup_RX(ADXL_345);
+  Receive();
+  while (UCB0CTL1 & UCTXSTP);             // Ensure stop condition got sent
+
+  acc[0] = ((((int)RxBuffer[1]) << 8) | RxBuffer[0]) << 6;
+  acc[1] = ((((int)RxBuffer[3]) << 8) | RxBuffer[2]) << 6;
+  acc[2] = ((((int)RxBuffer[5]) << 8) | RxBuffer[4]) << 6;
+    
+  // now You have XYZ axis reading in x1,x2,x3 variable....Bingo... you can play with it as you like....
+  // Below if sense x and y angle and Red led is on if its more then 45 or less then -45...
+  // you can put your own condition here...
+
+  /* if ((acc[0] > 0x2000) || (acc[1] > 0x2000) || (acc[0] < -0x2000) || (acc[1] < -0x2000)) { */
+  /*   P1OUT |= BIT0; // red led on */
+  /* } */
+  /* else { */
+  /*   P1OUT &= ~BIT0; // red led off */
+  /* } */
+
+  Setup_UART();
+  /* UARTSendArray("sample\n", 7); */
+  /* UARTSendInt(acc[0]); */
+  /* UARTSendInt(acc[1]); */
+  /* UARTSendInt(acc[2]); */
+  hmm();
 }
 
 //-------------------------------------------------------------------------------
@@ -190,34 +247,21 @@ __interrupt void USCIAB0TX_ISR(void)
       }
     else
       {
-        if(RPT_Flag == 0)
-          UCB0CTL1 |= UCTXSTP;                // No Repeated Start: stop condition
-        if(RPT_Flag == 1){                    // if Repeated Start: do nothing
-          RPT_Flag = 0;
-        }
+        UCB0CTL1 |= UCTXSTP;                // No Repeated Start: stop condition
         *PRxData++ = UCB0RXBUF;                   // Move final RX data to PRxData
         __bic_SR_register_on_exit(CPUOFF);      // Exit LPM0
       }}
   else{                                     // Master Transmit
     if (TXByteCtr)                        // Check TX byte counter
       {
-        UCB0TXBUF = MSData[TXByteCtr];          // Load TX buffer
         TXByteCtr--;                            // Decrement TX byte counter
+        UCB0TXBUF = MSData[TXByteCtr];          // Load TX buffer
       }
     else
       {
-        /* UCB0CTL1 |= UCTXSTP;                    // I2C stop condition */
-        /* IFG2 &= ~UCB0TXIFG;                     // Clear USCI_B0 TX int flag */
-        if(RPT_Flag == 1){
-          RPT_Flag = 0;
-          TXByteCtr = NUM_BYTES_TX;                // Load TX byte counter
-          __bic_SR_register_on_exit(CPUOFF);
-        }
-        else{
           UCB0CTL1 |= UCTXSTP;                    // I2C stop condition
           IFG2 &= ~UCB0TXIFG;                     // Clear USCI_B0 TX int flag
           __bic_SR_register_on_exit(CPUOFF);      // Exit LPM0
-        }
       }
   }
 }
@@ -266,8 +310,8 @@ void Setup_RX(unsigned char Dev_ID){
 }
 
 void Transmit(unsigned char Reg_ADD,unsigned char Reg_DAT){
-  MSData[2]= Reg_ADD;
-  MSData[1]= Reg_DAT;
+  MSData[1]= Reg_ADD;
+  MSData[0]= Reg_DAT;
   TXByteCtr = NUM_BYTES_TX;                  // Load TX byte counter
   while (UCB0CTL1 & UCTXSTP);             // Ensure stop condition got sent
   UCB0CTL1 |= UCTR + UCTXSTT;             // I2C TX, start condition
@@ -275,7 +319,7 @@ void Transmit(unsigned char Reg_ADD,unsigned char Reg_DAT){
 }
 
 void TransmitOne(unsigned char Reg_ADD){
-  MSData[1]= Reg_ADD;
+  MSData[0]= Reg_ADD;
   TXByteCtr = 1;                  // Load TX byte counter
   while (UCB0CTL1 & UCTXSTP);             // Ensure stop condition got sent
   UCB0CTL1 |= UCTR + UCTXSTT;             // I2C TX, start condition
@@ -338,19 +382,16 @@ char filter(){
   fp_t abs = fp_add(fp_add(fp_mul(acc[0], acc[0]),
                            fp_mul(acc[1], acc[1])),
                     fp_mul(acc[2], acc[2]));
-  //printf("abs = %.30f\n", fp2d(abs));
-  UARTSendArray("abs=", 4);
-  UARTSendInt(abs);
   ////////////////////////////////////////////////////////////////////////////////
   //idle state filter
-  if (!(fp_cmp(abs, d2fp(0.31))==1 ||
-        fp_cmp(abs, d2fp(0.28))==-1)) { // if between 0.01 - 0.09
+  if (!(fp_cmp(abs, d2fp(0.32))==1 ||
+        fp_cmp(abs, d2fp(0.27))==-1)) { // if between 0.01 - 0.09
     return false;
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   // def = directional equivalence filter
-  fp_t def_sensitivity = d2fp(0.04);
+  fp_t def_sensitivity = d2fp(0.08);
   if (fp_cmp(acc[0], fp_sub(dir_filter_ref[0], def_sensitivity))==-1 ||
       fp_cmp(acc[0], fp_add(dir_filter_ref[0], def_sensitivity))== 1 ||
       fp_cmp(acc[1], fp_sub(dir_filter_ref[1], def_sensitivity))==-1 ||
@@ -368,7 +409,7 @@ char filter(){
 //The quantizer, maps accelerometer readings to a set of integers.
 char derive_group(){
   fp_t a, b, c, d;
-  fp_t minDist = 0x7f; //0x7fff;
+  fp_t minDist = 0x7fff; //0x7fff;
   char minGroup=0;
   fp_t *ref;
   char i;
@@ -384,8 +425,8 @@ char derive_group(){
       minGroup = i;
     }
   }
-  UARTSendArray("group=", 6);
-  UARTSendInt(minGroup);
+  /* UARTSendArray("group=", 6); */
+  /* UARTSendInt(minGroup); */
   return minGroup;
 }
 
@@ -396,56 +437,29 @@ fp_t forward_proc_inc(char o){
   char k,l;
 
   if (started == false){
-    UARTSendArray("init\n", 5);
     for (l = 0; l < 8; l++){
       f[l] = b[(o<<3) + l]; // pi*b
+      //f[l] = b[o][l];
     }
     for (l = 1; l < 8; l++){
       f[l] = 0;
     }
-    started = true;
     return 0;
   }else{
-    UARTSendArray("proc\n", 5);
     for (k = 0; k < 8; k++){
       sum = 0;
       for (l = 0; l < 8; l++){
-        sum = fp_add(sum, fp_mul(s[l], a[l][k]));
+        //sum = fp_add(sum, fp_mul(s[l], a[(l<<3) + k]));
+        sum = fp_add(sum, fp_mul(s[l], b[(l<<3) +k]));
       }
       f[k] = fp_mul(sum, b[(o<<3)+k]);
+      //f[k] = fp_mul(sum, b[o][k]);
       ord |= f[k];
     }
   }
   return ord;
-  }
-
-//Called with each accelerometer reading
-void input_reading(){
-  fp_t ord = 0;
-  char i,l;
-  if (filter()){
-    for (i = 0; i < 2; i++){
-      char group = derive_group();
-      ord |= forward_proc_inc(group);
-    }
-
-    //counts the number of bits we can shift left by - the leading zeros
-    // : count 0
-    // : count1 over if 2* over 1 . + count1 ; then drop ;
-    char n = 0;
-    while (ord && !(ord & last_bit)){
-      n += 1;
-      ord = ord << 1;
-    }
-    if (n>3){
-      n-=3;
-      for (l = 0; l < 8; l++){
-        s[l] = f[l] << n;
-        f[l] = s[l] << n;
-      }
-    }
-  }
 }
+
 
 //Called at the end of the gesture,
 //Returns the id of the recognized gesture or -1 if none.
@@ -471,21 +485,62 @@ char input_end(){
   
   //printf("m->prob = %.30f\n", fp2d(recogprob));
   
+  UARTSendArray("p=", 2);
+  UARTSendInt(recogprob);
   //dir_filter_ref[0] = 0; //reset for next time
   return recognized;
 }
 
+//int pass = 0;
+
+//Called with each accelerometer reading
+void input_reading(){
+  fp_t ord = 0;
+  char i,l;
+  if (filter()){
+    //pass++;
+    for (i = 0; i < 2; i++){
+      char group = derive_group();
+      ord |= forward_proc_inc(group);
+    }
+
+    //counts the number of bits we can shift left by - the leading zeros
+    char n = 0;
+    while(ord > 0){
+      n += 1;
+      ord = ord << 1;
+    }
+    if (n>4) {
+      n -= 4;
+    }
+    else {
+      n = 0;
+    }
+    for (l = 0; l < 8; l++) {
+      s[l] = f[l] << n;
+      f[l] = s[l] << n;
+    }
+    if (started == false) started = true;
+    // constantly moving ~35 s for 1000 iter   => 29 iter/s => 34 ms
+    // one gestuer => 5 s
+  } else {
+    //__delay_cycles(1000);  // delay 1 ms
+    // 3.9 s
+  }
+}
+
 int iter = 0;
 void hmm() {
-  UARTSendArray("iter=", 5);
-  UARTSendInt(iter);
   iter++;
   input_reading();
-  if(iter == 20) {
-    iter = 0;
+  if(iter == 1000) {
     char out = input_end();
-    UARTSendArray("out=", 4);
+    //UARTSendArray("out=", 4);
     UARTSendInt(out);
+    /* UARTSendArray("pass=", 5); */
+    /* UARTSendInt(pass); */
+    /* pass = 0; */
+    iter = 0;
+    //__delay_cycles(1000);  // delay 1 ms
   }
-  UARTSendArray("\n", 1);
 }
